99e06eda963b5971bffadec7a63c124fcd802640
darango <markus48@gmail.com>
2022-01-09 10:17:36 +0100

    add settings

--- menusio-p.js
+++ menusio-p.js
@@ -1,8 +1,6 @@
 const paragraph = document.getElementsByTagName("p");
 const text = ("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin in tellus sit amet nibh dignissim sagittis. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat. Proin pede metus, vulputate nec, fermentum fringilla, vehicula vitae, justo. In enim a arcu imperdiet malesuada. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Aenean vel massa quis mauris vehicula lacinia. Mauris dictum facilisis augue. Etiam ligula pede, sagittis quis, interdum ultricies, scelerisque eu. Proin mattis lacinia justo. Donec quis nibh at felis congue commodo. Etiam quis quam. Fusce aliquam vestibulum ipsum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.");
 
-for(let i = 0; i < paragraph.length; i++){
+for (let i = 0; i < paragraph.length; i++) {
     paragraph[i].textContent = (text);
-    console.log(paragraph[i]);
-
 }
--- menusio.js
+++ menusio.js
@@ -1,23 +1,25 @@
 var menusio = menusio || ((() => {
-    let _args = {};
     return {
-        init(Args) {
-            _args = Args;
-        },
-        build() {
-            const menuList = document.getElementById(_args[1]);
-            const main = document.getElementById(_args[0]);
-            const list = main.getElementsByTagName("h2");
+        build(arg) {
+            const main = document.getElementById(arg.idArticle);
+            if (main !== null) {
+                const list = main.querySelectorAll(arg.selectors);
+                const menuList = document.getElementById(arg.idList);
 
-            for (let i = 0; i < list.length; i++) {
-                const link = document.createElement("a");
-                const li = document.createElement("li");
-                list[i].id = i + 1;
-                link.setAttribute("href", `#${list[i].id}`);
-                link.setAttribute("class", "js-anchor");
-                link.innerHTML = list[i].textContent;
-                li.appendChild(link);
-                menuList.appendChild(li);
+                for (let i = 0; i < list.length; i++) {
+                    const link = document.createElement("a");
+                    const li = document.createElement("li");
+
+                    list[i].id = i + 1;
+                    link.setAttribute("href", `#${list[i].id}`);
+
+                    link.innerHTML = list[i].innerHTML;
+                    list[i].innerHTML = link.outerHTML;
+
+                    link.innerHTML = list[i].textContent;
+                    li.appendChild(link);
+                    menuList.appendChild(li);
+                }
             }
         }
     };
